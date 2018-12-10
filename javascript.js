0128dab9b56a9f838882b8696bae81cc1d83f336
Max <frostysike20@gmail.com>
2018-12-10 09:14:36 +0100

    cd

--- javascript.js
+++ javascript.js
@@ -32,3 +32,29 @@ function headerScroll() {
 function slide(event){
     event.target.previousElementSibling.style.clip = "rect(0px, "+(event.clientX-event.target.offsetLeft)+"px, 427px, 0px)";
 }
+
+
+
+var countDownDate = new Date("April 5, 2019 00:00:00").getTime();
+
+var x = setInterval(function() {
+
+
+var now = new Date().getTime();
+var distance = count - now;
+
+var days = Math.floor(distance/(1000*60*60*24));
+var hours = Math.floor((distance%(1000*60*60*24))/(1000*60*60));
+var minutes = Math.floor((distance%(1000*60*60))/(1000*60));
+var seconds = Math.floor((distance%(1000*60))/1000);
+
+document.getElementById("days").innerHTML = days;
+document.getElementById("hours").innerHTML = hours;
+document.getElementById("min").innerHTML = minutes;
+document.getElementById("sekunder").innerHTML = seconds;
+
+
+if(distance <= 0) {
+  clearInterval(x);
+}
+},1000);
