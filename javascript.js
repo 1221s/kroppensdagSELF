2e1ea523d78cbbb30fe4b69ef47888b9d11950c0
1221s <42850946+1221s@users.noreply.github.com>
2018-12-12 21:36:38 +0100

    fixed shit

--- javascript.js
+++ javascript.js
@@ -10,8 +10,10 @@ function headerScroll() {
         document.getElementById("headerItem2").classList.add("headerItemSmall");
         document.getElementById("headerItem3").classList.add("headerItemSmall");
         document.getElementById("headerItem4").classList.add("headerItemSmall");
+        document.getElementById("headerItem5").classList.add("headerItemSmall");
 
         document.getElementById("headerVideoText").style.transitionDelay="0s";
+        document.getElementById("headerHomeText").style.transitionDelay="0s";
         document.getElementById("headerBilledeText").style.transitionDelay="0s";
         document.getElementById("headerForumText").style.transitionDelay="0s";
         document.getElementById("headerProgramText").style.transitionDelay="0s";
@@ -22,40 +24,60 @@ function headerScroll() {
       document.getElementById("headerItem2").classList.remove("headerItemSmall");
       document.getElementById("headerItem3").classList.remove("headerItemSmall");
       document.getElementById("headerItem4").classList.remove("headerItemSmall");
+      document.getElementById("headerItem5").classList.remove("headerItemSmall");
 
       document.getElementById("headerVideoText").style.transitionDelay="0.4s";
       document.getElementById("headerBilledeText").style.transitionDelay="0.4s";
       document.getElementById("headerForumText").style.transitionDelay="0.4s";
       document.getElementById("headerProgramText").style.transitionDelay="0.4s";
+      document.getElementById("headerHomeText").style.transitionDelay="0.4s";
     }
 }
+//window.onscroll = function(){
+//window.scrollBy(0, 0);
+//}
 
-window.scrollBy(0, window.innerHeight);
 // Laura Jscript
 
-function slide(event){
-    event.target.previousElementSibling.style.clip = "rect(0px, "
-    +(event.clientX-event.target.offsetLeft)+"px, 427px, 0px)";
+//function slide(event){
+  //  event.target.previousElementSibling.style.clip = "rect(0px, "
+    //+(event.clientX-event.target.offsetLeft)+"px, 427px, 0px)";
+//}
+
+// SLIDESHOW
+var slideIndex = 0;
+showSlides();
+
+function showSlides() {
+var i;
+var slides = document.getElementsByClassName("mySlides");
+for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "block";
+}
+slideIndex++;
+if (slideIndex > slides.length) {slideIndex = 1}
+slides[i-1].style.display = "none";
+setTimeout(showSlides, 1000); // Change image every 2 seconds
 }
 
 // Her start max Countdown
-var countDownDate = new Date("Apr 5, 2019 00:00:00").getTime();
+//var countDownDate = new Date("Apr 5, 2019 00:00:00").getTime();
 
-var x = setInterval(function() {
-
-
-var now = new Date().getTime();
-var distance = count - now;
-
-var days = Math.floor(distance/(1000*60*60*24));
-
-document.getElementById("days").innerHTML = days;
+//var x = setInterval(function() {
 
 
-if(distance <= 0) {
-  clearInterval(x);
-}
-},1000);
+//var now = new Date().getTime();
+//var distance = count - now;
+
+//var days = Math.floor(distance/(1000*60*60*24));
+
+//document.getElementById("days").innerHTML = days;
+
+
+//if(distance <= 0) {
+//  clearInterval(x);
+//}
+//},1000);
 
 
 //Her Starter Lightbox JS//
